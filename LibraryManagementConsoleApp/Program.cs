7d39d1e5befd545daf501ff141eda9a9e09720e4
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-05-23 12:48:13 +0330

    Main view and app options created.

--- LibraryManagementConsoleApp/Program.cs
+++ LibraryManagementConsoleApp/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Library Management Console App");
+﻿Console.WriteLine("-----------------------------------------------------------");
+Console.WriteLine("Welcome to Library Management Console App");
+Console.WriteLine("-----------------------------------------------------------");
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("------------------- Application options -------------------");
+Console.WriteLine("1. Member List");
+Console.WriteLine("2. Book List");
+Console.WriteLine("3. Add new member");
+Console.WriteLine("4. Add new book");
+Console.WriteLine("5. Borrow book");
+Console.WriteLine("6. Return book");
+Console.WriteLine("7. Exit");
+Console.WriteLine("-----------------------------------------------------------");
+
+Console.Write("Please select your option: ");
+
+while (true)
+{
+    var option = Console.ReadLine();
+    
+    Console.Clear();
+
+    switch (option)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "6":
+            break;
+        case "7":
+            break;
+        default:
+            break;
+    }
+}
